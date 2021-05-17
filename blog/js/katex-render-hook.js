1499191640c589e0661516263ecfc14a3304561e
Koji Miyazato <viercc@gmail.com>
2021-05-17 11:36:10 +0900

    Publish.

---
+++ blog/js/katex-render-hook.js
@@ -0,0 +1,19 @@
+'use strict';
+document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(document.body, {
+        // customised options
+        // • auto-render specific keys, e.g.:
+        delimiters: [
+            {left: '$$', right: '$$', display: true},
+            {left: '$', right: '$', display: false},
+            {left: '\\(', right: '\\)', display: false},
+            {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+            {left: "\\begin{align}", right: "\\end{align}", display: true},
+            {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+            {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+            {left: "\\begin{CD}", right: "\\end{CD}", display: true},
+        ],
+        // • rendering keys, e.g.:
+        throwOnError : false
+    });
+});
