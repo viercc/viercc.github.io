ada3de7af2cf19a8b99c50c880300404bbe19e46
Koji Miyazato <viercc@gmail.com>
2021-05-18 17:22:57 +0900

    Changes to generator
    * List only 15 recent posts at top page
    * Do not load KaTeX when there's no math: in metadata

--- site-too-lazy-to-evaluate/site.hs
+++ site-too-lazy-to-evaluate/site.hs
@@ -1,29 +1,49 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
 import           Hakyll
 import           Hakyll.Web.Pandoc
-import           Text.Pandoc.Options
+import Text.Pandoc.Options
 
---------------------------------------------------------------------------------
+------------------------------------
 
-pandocCompiler' :: Compiler (Item String)
-pandocCompiler' = pandocCompilerWith
-  defaultHakyllReaderOptions
-    { readerExtensions = extensions' }
-  defaultHakyllWriterOptions
-    { writerExtensions = extensions'
-    , writerHTMLMathMethod = htmlMath }
+
+defaultOptions :: (ReaderOptions, WriterOptions)
+defaultOptions = (reader', writer')
   where
-    extensions' =
-      enableExtension Ext_footnotes $
-      enableExtension Ext_tex_math_double_backslash $
-      disableExtension Ext_tex_math_dollars $
-      readerExtensions defaultHakyllReaderOptions
+    ext =
+      enableExtension Ext_footnotes .
+      disableExtension Ext_tex_math_dollars
 
-htmlMath :: HTMLMathMethod
--- htmlMath = MathML
-htmlMath = KaTeX defaultKaTeXURL
+    reader = defaultHakyllReaderOptions
+    reader' = reader{ readerExtensions = ext (readerExtensions reader)}
+    
+    writer = defaultHakyllWriterOptions
+    writer' = writer{ writerExtensions = ext (writerExtensions writer)}
+
+setMathOpt ::
+     Maybe HTMLMathMethod
+  -> (ReaderOptions, WriterOptions)
+  -> (ReaderOptions, WriterOptions)
+setMathOpt Nothing opts = opts
+setMathOpt (Just htmlMath) (reader, writer) = (reader', writer')
+  where
+    mathExts = enableExtension Ext_tex_math_double_backslash
+
+    rx = readerExtensions reader
+    reader' = reader{ readerExtensions = mathExts rx }
+    wx = writerExtensions writer
+    writer' = writer{
+        writerExtensions = mathExts wx
+      , writerHTMLMathMethod = htmlMath
+      }
+
+pandocCompiler' :: Maybe HTMLMathMethod -> Compiler (Item String)
+pandocCompiler' mathOpt = pandocCompilerWith readerOpt writerOpt
+  where
+    (readerOpt, writerOpt) = setMathOpt mathOpt defaultOptions
+
+katex :: HTMLMathMethod
+katex = KaTeX defaultKaTeXURL
 
 main :: IO ()
 main = hakyll $ do
@@ -46,13 +66,13 @@ main = hakyll $ do
     
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
-        compile $ pandocCompiler'
+        compile $ pandocCompiler' Nothing
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler'
+        compile $ pandocCompiler' (Just katex)
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
@@ -60,26 +80,25 @@ main = hakyll $ do
     create ["archive.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            postsAll <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+                     listField "posts" postCtx (return postsAll)
+                  <> constField "title" "Archive"
+                  <> defaultContext
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
-
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts15 <- fmap (take 15) $ recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+                     listField "posts" postCtx (return posts15)
+                  <> constField "title" "Home"
+                  <> defaultContext
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
@@ -88,9 +107,8 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateBodyCompiler
 
-
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+     dateField "date" "%F"
+  <> defaultContext
