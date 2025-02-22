b598125c7a53f43ad273f939f8b9968cf5df9a29
Koji Miyazato <viercc@gmail.com>
2025-02-22 17:57:50 +0900

    Publish.

--- blog/js/katex-render-hook.js
+++ blog/js/katex-render-hook.js
@@ -6,17 +6,35 @@ document.addEventListener("DOMContentLoaded", function() {
             NodeFilter.SHOW_ELEMENT,
             (n) => n.classList.contains("math")
         );
+    const mathBlockElements =
+        document.createNodeIterator(
+            document.body,
+            NodeFilter.SHOW_ELEMENT,
+            (n) => n.classList.contains("math-block")
+        );
     let macros = {};
-    while (1) {
-        let elem = mathElements.nextNode();
-        if (!elem) break;
-        
-        const isDisplay = elem.classList.contains("display");
-        katex.render(elem.firstChild.data, elem, {
+    let renderElem = (content, targetElem, isDisplay) => {
+        katex.render(content, targetElem, {
             "displayMode": isDisplay,
             "macros": macros,
             "throwOnError": false,
             "errorColor": "#DD0000"
         });
+    };
+    while (1) {
+        let elem = mathBlockElements.nextNode();
+        if (!elem) break;
+        let insertedElem = document.createElement("span");
+        elem.parentNode.insertBefore(insertedElem, elem);
+        elem.childNodes.forEach((childElem) => {
+            if (childElem.nodeType === Node.ELEMENT_NODE && childElem.tagName === "CODE") {
+                renderElem(childElem.textContent, insertedElem, true);
+            }
+        });
+    }
+    while (1) {
+        let elem = mathElements.nextNode();
+        if (!elem) break;
+        renderElem(elem.textContent, elem, elem.classList.contains("display"));
     }
 });
